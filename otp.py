c504374548e61a3850899b5b3ec2d8200e281aa6
Rafael Z Tanganelli <kabelu@gmail.com>
2023-04-24 15:31:23 +0200

    starting next project

---
+++ otp.py
@@ -0,0 +1,41 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    otp.py                                             :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: rzamolo- <rzamolo-@student.42.fr>          +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2023/04/24 11:22:09 by rzamolo-          #+#    #+#              #
+#    Updated: 2023/04/24 11:23:34 by rzamolo-         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+# How to parse arguments from the command line
+import argparse
+
+if __name__ == "__main__":
+    # Create the parser
+	my_parser = argparse.ArgumentParser(description='Encrypt or decrypt using OTP')
+
+	# Add the arguments
+	my_parser.add_argument('Message',
+							metavar='message',
+							type=str,
+							help='the message to encrypt or decrypt')
+
+	my_parser.add_argument('Key',
+							metavar='key',
+							type=str,
+							help='the encryption key')
+
+	my_parser.add_argument('Action',
+							metavar='action',
+							type=str,
+							help='the action to perform (encrypt or decrypt)')
+
+	# Execute the parse_args() method
+	args = my_parser.parse_args()
+
+	print('Message: ', args.Message)
+	print('Key: ', args.Key)
+	print('Action: ', args.Action)
