828ce84094045b22f64017f77a28f03330ce92f3
Rafael Z Tanganelli <kabelu@gmail.com>
2023-04-27 15:57:14 +0200

    bleh

--- using_totp.py
+++ using_totp.py
@@ -6,31 +6,47 @@
 #    By: rzamolo- <rzamolo-@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/04/25 12:02:37 by rzamolo-          #+#    #+#              #
-#    Updated: 2023/04/25 13:05:17 by rzamolo-         ###   ########.fr        #
+#    Updated: 2023/04/27 15:40:59 by rzamolo-         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
-import hmac, base64, struct, hashlib, time
+import hmac, base64, struct, hashlib, time, math
 
-def get_hotp_token(secret, intervals_no):
-    key = base64.b32decode(secret, True)
-    #decoding our key
-    msg = struct.pack(">Q", intervals_no)
-    #conversions between Python values and C structs represente
-    h = hmac.new(key, msg, hashlib.sha1).digest()
-    o = o = h[19] & 15
-    #Generate a hash using both of these. Hashing algorithm is HMAC
-    h = (struct.unpack(">I", h[o:o+4])[0] & 0x7fffffff) % 1000000
-    #unpacking
-    return h
+# TODO: Test input (ishexa and lenght > 64)
+# TODO: Generate OTP (30 sec.)
 
-def get_totp_token(secret):
-    #ensuring to give the same otp for 30 seconds
-    x =str(get_hotp_token(secret,intervals_no=int(time.time())//30))
-    #adding 0 in the beginning till OTP has 6 digits
-    while len(x)!=6:
-        x+='0'
-    return x#base64 encoded key
+import random
 
-secret = 'MNUGC2DBGBZQ===='
-print(get_totp_token(secret))
+def generate_key(length):
+    """Generate a random binary key of a given length."""
+    key = []
+    for i in range(length):
+        key.append(random.randint(0, 1))
+    return key
+
+def otp_encrypt(plaintext, key):
+    """Encrypt plaintext using the OTP algorithm."""
+    ciphertext = []
+    for i in range(len(plaintext)):
+        ciphertext.append(plaintext[i] ^ key[i])
+    return ciphertext
+
+def otp_decrypt(ciphertext, key):
+    """Decrypt ciphertext using the OTP algorithm."""
+    plaintext = []
+    for i in range(len(ciphertext)):
+        plaintext.append(ciphertext[i] ^ key[i])
+    return plaintext
+
+# Example usage
+plaintext = ["1","0","1","0","0","1", "0", "0"]
+# plaintext = " ".join(map(bin, bytearray(plaintext, "utf-8"))) # Binary representation of "Hello"
+print(plaintext)
+key = generate_key(len(plaintext))
+ciphertext = otp_encrypt(plaintext, key)
+decrypted_plaintext = otp_decrypt(ciphertext, key)
+
+print("Plaintext:", plaintext)
+print("Key:", key)
+print("Ciphertext:", ciphertext)
+print("Decrypted plaintext:", decrypted_plaintext)
